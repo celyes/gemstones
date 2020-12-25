977bf71f330dea66f7495725fdaa7d5777fcac12
celyes <celyes02@gmail.com>
2020-12-25 18:29:18 +0100

    update arrays.rb

--- src/basics/arrays.rb
+++ src/basics/arrays.rb
@@ -1,5 +1,7 @@
 =begin
 
+cities =['Djelfa', 'Algiers', 'Oran', 'Constantine'];
+
 puts cities.first # Djelfa
 puts cities.last # Constantine
 puts cities[2] # Oran 
@@ -9,9 +11,15 @@ puts cities
 cities.pop # removes Setif from the array and prints it to the console
 puts "========="
 puts cities
-=end
-
-cities =['Djelfa', 'Algiers', 'Oran', 'Constantine'];
 
 upcities = cities.map { |city| city.upcase }
 puts upcities
+
+=end
+
+# select only numbers less than 5
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+
+puts numbers.select { |number| number < 5 } 
+
